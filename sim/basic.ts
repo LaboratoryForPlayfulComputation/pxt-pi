6d8e2167713ccc54a65f1e3233132dc5e21825d5
Michal Moskal <michal.moskal@microsoft.com>
2016-05-06 13:06:44 +0100

    Auto-generate sims.d.ts

--- sim/basic.ts
+++ sim/basic.ts
@@ -1,8 +1,13 @@
 /// <reference path="../libs/sample/enums.d.ts"/>
 
 namespace pxsim.basic {
-    export function forward(steps: number) {
-        let cb = getResume();
+    /**
+     * Moves the sprite forward
+     * @param steps number of steps to move, eg: 1
+     */
+    //% weight=90
+    //% blockId=sampleForward block="forward %steps"
+    export function forwardAsync(steps: number) {
         let b = board();
 
         let deg = b.sprite.angle / 180 * Math.PI;
@@ -10,20 +15,42 @@ namespace pxsim.basic {
         b.sprite.y += Math.sin(deg) * steps * 10;
 
         b.updateView();
-        setTimeout(cb, 400);
+        return Promise.delay(400)
     }
 
-    export function turn(direction: Direction, angle: number) {
-        let cb = getResume();
+    /**
+     * Moves the sprite forward
+     * @param angle degrees to turn, eg:90
+     */
+    //% weight=85
+    //% blockId=sampleTurn block="turn %direction|by %angle degrees"
+    export function turnAsync(direction: Direction, angle: number) {
         let b = board();
 
         if (direction == Direction.Left)
             b.sprite.angle -= angle;
         else
             b.sprite.angle += angle;
-        setTimeout(cb, 400);
+        return Promise.delay(400)
     }
 
-    export var pause = thread.pause;
-    export var forever = thread.forever;
+    /**
+     * Repeats the code forever in the background. On each iteration, allows other code to run.
+     * @param body TODO
+     */
+    //% help=functions/forever weight=55 blockGap=8
+    //% blockId=device_forever block="forever" icon="\uf01e" 
+    export function forever(body: RefAction): void { 
+        thread.forever(body)
+    }
+
+    /**
+     * Pause for the specified time in milliseconds
+     * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
+     */
+    //% help=functions/pause weight=54
+    //% block="pause (ms) %pause" blockId=device_pause icon="\uf110"
+    export function pauseAsync(ms: number) { 
+        return Promise.delay(ms)
+    }
 }
