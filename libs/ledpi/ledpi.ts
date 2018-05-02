8d533f4b9a2e4f92f838c904a8df8ca2c745331a
aeos42 <aeos42@gmail.com>
2018-05-02 15:12:10 -0600

    expanded LED block

--- libs/ledpi/ledpi.ts
+++ libs/ledpi/ledpi.ts
@@ -15,20 +15,26 @@ namespace ledpi {
         //% blockId=piLedOn block="set LED at %this %on"
         //% on.fieldEditor=toggleonoff
         ledToggle(on: boolean) {
-           
+            if (on) mypi.piCall("setComponentValue", "ledOn", this.options)
+            else mypi.piCall("setComponentValue", "ledOff", this.options)
         }
     }
 
-    //% fixedInstance block="led 1"
-    export const port1 = new ledPi({});
-    //% fixedInstance block="led 2"
-    export const port2 = new ledPi({});
-    //% fixedInstance block="led 3"
-    export const port3 = new ledPi({});
-    //% fixedInstance block="led 4"
-    export const port4 = new ledPi({});
-    //% fixedInstance block="led 5"
-    export const port5 = new ledPi({});
+    //% fixedInstance block="port 2"
+    export const port2 = new ledPi({port: 2});
+    //% fixedInstance block="port 3"
+    export const port3 = new ledPi({port: 3});
+    //% fixedInstance block="port 4"
+    export const port4 = new ledPi({port: 4});
+    //% fixedInstance block="port 5"
+    export const port5 = new ledPi({port: 5});
+    //% fixedInstance block="port 6"
+    export const port6 = new ledPi({port: 6});
+    //% fixedInstance block="port 7"
+    export const port7 = new ledPi({port: 7});
+    //% fixedInstance block="port 8"
+    export const port8 = new ledPi({port: 8});
+    
     
     
 }
