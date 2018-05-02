9e52e8921a1af5bcac44dd71aa94cc05cd9a5faa
aeos42 <aeos42@gmail.com>
2018-05-02 15:01:39 -0600

    grove config blocks are finished and send appropriate messages

--- libs/groveconfig/groveconfig.ts
+++ libs/groveconfig/groveconfig.ts
@@ -1,32 +1,50 @@
 enum AnalogDeviceType {
-    //% block="led"
-    Led,
-    //% block="ultrasonic sensor"
-    UltrasonicSensor,
+    //% block="light sensor"
+    lightSensor,
+    //% block="sound sensor"
+    soundSensor,
     //% block="moisture"
-    Moisture
+    moistureSensor,
+    //% block="slide potentiometer"
+    slidePot
 }
 
 enum DigitalDeviceType {
     //% block="button"
-    Button,
+    button,
     //% block="water sensor"
-    WaterSensor
+    waterSensor,
+    //% block="buzzer"
+    buzzer,
+    //% block="LED"
+    LED,
+    //% block="relay"
+    relay,
+    //% block="temp/humidity sensor"
+    temHum,
+    //% block="ultrasonic sensor"
+    ultrasonic
 }
-
 
 namespace groveconfig {
 
 
     const analogDeviceDict = [
-        "led",
-        "ultrasonic sensor",
-        "moisture"
+        "lightSensor",
+        "soundSensor",
+        "moistureSensor",
+        "slidePot"
+        
     ];
 
     const digitalDeviceDict = [
         "button",
-        "water sensor"
+        "water sensor",
+        "buzzer",
+        "LED",
+        "relay",
+        "tempHum",
+        "ultrasonic"
     ];
 
     /**
@@ -46,12 +64,12 @@ namespace groveconfig {
             mypi.piCall("setComponentType", analogDeviceDict[type], this.options);
         }
     }
+    //% fixedInstance block="Analog Port 0"
+    export const aPort1 = new AnalogPort({port: 0});
     //% fixedInstance block="Analog Port 1"
-    export const aPort1 = new AnalogPort({port: 1});
+    export const aPort2 = new AnalogPort({port: 1});
     //% fixedInstance block="Analog Port 2"
-    export const aPort2 = new AnalogPort({port: 2});
-    //% fixedInstance block="Analog Port 3"
-    export const aPort3 = new AnalogPort({port: 3});
+    export const aPort3 = new AnalogPort({port: 2});
 
     /**
      * digport
@@ -72,14 +90,12 @@ namespace groveconfig {
     }
     //% fixedInstance block="Digital Port 2"
     export const dPort2 = new DigitalPort({port: 2});
-    //% fixedInstance block="Digital Port 3"
-    export const dPort3 = new DigitalPort({port: 3});
     //% fixedInstance block="Digital Port 4"
     export const dPort4 = new DigitalPort({port: 4});
-    //% fixedInstance block="Digital Port 7" 
-    export const dPort7 = new DigitalPort({port: 5});
-    //% fixedInstance block="Digital Port 8"
-    export const dPort8 = new DigitalPort({port: 6});
+    //% fixedInstance block="Digital Port 7"
+    export const dPort7 = new DigitalPort({port: 7});
+    //% fixedInstance block="Digital Port 8" 
+    export const dPort8 = new DigitalPort({port: 8});
 
     /**
      * digPWMport
