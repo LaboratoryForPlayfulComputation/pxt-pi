97cade4a20b0d3f13982e9615ef6a7040010dd95
William Temple <William.Temple@colorado.edu>
2018-08-31 16:20:28 -0600

    Save code by post to /api/save. GrovePi library stubbed out.
    Signed-off-by: William Temple <William.Temple@colorado.edu>

--- editor/extension.ts
+++ editor/extension.ts
@@ -1,17 +1,17 @@
 /// <reference path="../node_modules/pxt-core/built/pxteditor.d.ts" />
 
 namespace pxt.editor {
-    const _PORT = 3074;
-    const _HOST = "localhost";
-    const _PATH = "/save";
+    const _PORT = window.location.port;
+    const _HOST = window.location.hostname;
+    const _PATH = "/api/save";
     initExtensionsAsync = function (opts: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
         pxt.debug('loading pxt-pi target extensions...');
-        const res : pxt.editor.ExtensionResult = {
+        const res: pxt.editor.ExtensionResult = {
             saveProjectAsync: async (res: pxt.cpp.HexFile) => {
                 const payload = res.source;
                 console.log("sending: ", res);
-                return pxt.Util.httpPostJsonAsync(`http://${_HOST}:${_PORT}${_PATH}`, res.source)
-                    .then(() => {}); // Convert Promise<any> to Promise<void>
+		return pxt.Util.httpPostJsonAsync(`http://${_HOST}:${_PORT}${_PATH}`, res.source)
+                    .then(() => { }); // Convert Promise<any> to Promise<void>
             }
         };
         return Promise.resolve<pxt.editor.ExtensionResult>(res);
