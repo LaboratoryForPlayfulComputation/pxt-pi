e8aa61c31e41174ab143e0b77807cb4c988677d0
Tanner Quigley <TannerQuigley@me.com>
2019-02-27 12:52:49 -0700

    radio module
    Signed-off-by: Tanner Quigley <TannerQuigley@me.com>

---
+++ libs/radio/radio.ts
@@ -0,0 +1,11 @@
+//% block="Radio" weight=30 color=#e2008c icon="\uf012"
+namespace radio {
+
+    export class Packet {
+
+    }
+
+    //% blockId="onReciveNumber" block="on radio recived number"
+    export function onReciveNumber(handler: () => void): void { }
+
+}
