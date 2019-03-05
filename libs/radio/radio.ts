cdb56e8e197395ff820d4d5df04400ba76252a2e
Tanner Quigley <TannerQuigley@me.com>
2019-03-05 12:05:42 -0700

    on recive string and key value
    Signed-off-by: Tanner Quigley <TannerQuigley@me.com>

--- libs/radio/radio.ts
+++ libs/radio/radio.ts
@@ -8,4 +8,9 @@ namespace radio {
     //% blockId="onReciveNumber" block="on radio recived number"
     export function onReciveNumber(handler: () => void): void { }
 
+    //% blockId="onReciveKeyValue" block="on radio recived key value"
+    export function onReciveKeyValue(handler: () => void): void { }
+
+    //%blockId="onReciveString" block="on radio recived string"
+    export function onReciveString(handler: () => void): void { }
 }
