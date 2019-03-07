0158bc00e89d5353be726b9209c4bfbc43e1a74a
shadowfax6894 <anniekelly6894@gmail.com>
2019-03-06 18:11:31 -0700

    adding dmx ts files

---
+++ libs/dmx/dmx.ts
@@ -0,0 +1,11 @@
+//% block="DMX" weight=30 color=#7142f4 icon="\uf140"
+namespace dmx {
+
+    export class DMX {
+        constructor() { };
+    }
+
+    //% blockId="createFixture" block="create new fixture"
+    export function createFixture(): void { }    
+
+}
