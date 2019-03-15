c52efe29347d971bf215154747fd3bc8d83a5a86
shadowfax6894 <anniekelly6894@gmail.com>
2019-03-15 12:55:46 -0600

    reordered the blocks in the dmx drawer

--- libs/dmx/dmx.ts
+++ libs/dmx/dmx.ts
@@ -31,20 +31,22 @@ namespace dmx {
         Black = 0x000000    
     }
 
-    //% blockId=dmx_createfixture block="create fixture with %numChannels| channels" blockGap=8
-    //% blockSetVariable=fixture1
-    export function createFixture(numChannels: number): Fixture {
-        return new Fixture(); // just a stub
-    }
-
     /**
      * Create a new RGB light fixture
      * @param fixtureType of the RGB light, eg: RGBFixtureType.Baisun8ch
      */
     //% blockId=dmx_creatergbfixture block="create RGB light fixture %fixtureType" blockGap=8
     //% blockSetVariable=light1    
+    //% weight=100
     export function createRGBFixture(fixtureType: RGBFixtureType) : RGBFixture { 
         return new RGBFixture(); // just a stub
+    }
+
+    //% blockId=dmx_createfixture block="create fixture with %numChannels| channels" blockGap=8
+    //% blockSetVariable=fixture1
+    //% weight=99
+    export function createFixture(numChannels: number): Fixture {
+        return new Fixture(); // just a stub
     }
 
     //% blockNamespace=dmx
@@ -57,6 +59,7 @@ namespace dmx {
          * @param channel eg: 1
          */        
         //% blockId=fixture_updatechannel block="%dmx(fixture1) update channel %channel| to %value" blockGap=8
+        //% weight=96
         updateChannel(channel: number, value: number): void { }        
     }
 
@@ -73,6 +76,7 @@ namespace dmx {
          */        
         //% blockId=rgbfixture_setbrightness block="%dmx(light1) set brightness to %value" blockGap=8
         //% value.min=0 value.max= 255
+        //% weight=98
         setBrightness(value: number): void { }        
 
         /**
@@ -80,6 +84,7 @@ namespace dmx {
          * @param value 
          */        
         //% blockId=rgbfixture_setcolor block="%dmx(light1) set color to %value=dmx_colors" blockGap=8
+        //% weight=97
         setColor(value: number): void { }   
 
     }
