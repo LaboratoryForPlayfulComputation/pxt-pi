ae6dfd2edb58d90d5ac7c790e2bc4dc2ffdc3ef8
William Temple <William.Temple@colorado.edu>
2019-04-13 12:46:45 -0600

    Integrated visual changes and target modifications to use the new LPC PXT fork.
    Signed-off-by: William Temple <William.Temple@colorado.edu>

---
+++ docs/projects.md
@@ -0,0 +1,20 @@
+# Projects
+
+```codecard
+[
+    {
+        "name": "Examples",
+        "url": "/examples"
+    },
+    {
+        "name": "Tutorials",
+        "url": "/tutorials"
+    }
+]
+```
+
+## See Also
+
+[Examples](/examples),
+[Tutorials](/tutorials)
+
--- libs/timelapse/timelapse.ts
+++ libs/timelapse/timelapse.ts
@@ -1,4 +1,4 @@
-//% block="Timelapse" weight=50 color=#000000 icon="\uf017"
+//% block="Timelapse" weight=50 color=#0048EE icon="\uf017"
 namespace timelapse {
     //% blockId="timelapse.uploadImage" block="upload %image to my timelapse"
     export function uploadImage(image: webcam.Image) : void {}
